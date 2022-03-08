597b4b0b66101958984b0cc0c90e617cffde2de1
nedzie <marcos-elivelton@hotmail.com>
2022-03-08 16:21:31 -0300

    Add project files.

---
+++ ExercicioNove.ConsoleApp/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioNove.ConsoleApp
+{
+    internal class Aluno
+    {
+        public int quantiaDenotas;
+        public double[] notas;
+        public double mediaHarmonica;
+
+        public double ObterMediaHamonica(int quantiaDeNotas, double[] notas)
+        {
+            double somaTotalNotas = 0;
+            for (int i = 0; i < quantiaDenotas; i++)
+            {
+                somaTotalNotas += 1 / notas[i];
+            }
+            mediaHarmonica = 4 / somaTotalNotas;
+            return mediaHarmonica;
+        }
+    }
+}
---
+++ ExercicioNove.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExercicioNove.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno marcos = new Aluno();
+            Console.Write("Digite quantas notas vai atrelar ao aluno: \n> ");
+            marcos.quantiaDenotas = int.Parse(Console.ReadLine());
+            marcos.notas = new double[marcos.quantiaDenotas];
+            for (int i = 0; i < marcos.quantiaDenotas; i++)
+            {
+                Console.WriteLine("Informe a " + (i + 1) + "ª nota: ");
+                marcos.notas[i] = double.Parse(Console.ReadLine());
+            }
+            marcos.mediaHarmonica = marcos.ObterMediaHamonica(marcos.quantiaDenotas, marcos.notas);
+
+            Console.WriteLine("A média harmônica destas notas é de: " + marcos.mediaHarmonica.ToString("#.##") + ".");
+        }
+    }
+}
